7b30b32f27243547e86e7024011931cf468b18df
Christian Brel <ch.brel@gmail.com>
2014-09-05 00:12:15 +0200

    refs #3 : Add empty class to Clean and Init Database with some default data and some data for test development.

---
+++ app/scripts/CleanAndInitDatabase.ts
@@ -0,0 +1,25 @@
+/**
+ * @author Christian Brel <christian@the6thscreen.fr, ch.brel@gmail.com>
+ */
+
+/// <reference path="./core/Logger.ts" />
+
+/**
+ * Class to clean and Initialise Database with some data.
+ *
+ * @class CleanAndInitDatabase
+ */
+class CleanAndInitDatabase {
+
+    /**
+     * Method to clean and fulfill database with some data.
+     *
+     * @method run
+     */
+    run() {
+        //TODO
+    }
+}
+
+var caid = new CleanAndInitDatabase();
+caid.run();
