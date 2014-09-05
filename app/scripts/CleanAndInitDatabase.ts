fe591b7bbe587a67532ed4e4c683309ab8fc2793
Christian Brel <ch.brel@gmail.com>
2014-09-05 09:27:27 +0200

    refs #3 : Init cleaning of database.

--- app/scripts/CleanAndInitDatabase.ts
+++ app/scripts/CleanAndInitDatabase.ts
@@ -4,6 +4,23 @@
 
 /// <reference path="./core/Logger.ts" />
 
+/// <reference path="./model/ModelItf.ts" />
+/// <reference path="./model/SDI.ts" />
+/// <reference path="./model/Zone.ts" />
+/// <reference path="./model/Call.ts" />
+/// <reference path="./model/CallType.ts" />
+/// <reference path="./model/Profil.ts" />
+/// <reference path="./model/Timeline.ts" />
+/// <reference path="./model/InfoType.ts" />
+/// <reference path="./model/ParamValue.ts" />
+/// <reference path="./model/ParamType.ts" />
+/// <reference path="./model/ReceivePolicy.ts" />
+/// <reference path="./model/Renderer.ts"/>
+/// <reference path="./model/RenderPolicy.ts" />
+/// <reference path="./model/Role.ts" />
+/// <reference path="./model/Source.ts" />
+/// <reference path="./model/User.ts" />
+
 /**
  * Class to clean and Initialise Database with some data.
  *
@@ -11,12 +28,40 @@
  */
 class CleanAndInitDatabase {
 
+    static toClean = [Role, User];
+
     /**
      * Method to clean and fulfill database with some data.
      *
      * @method run
      */
     run() {
+        cleanAll();
+        fulfill();
+    }
+
+    /**
+     * Method to clean the database.
+     *
+     * @method cleanAll
+     */
+    cleanAll() {
+        for(var i in CleanAndInitDatabase.toClean) {
+            var modelToClean = CleanAndInitDatabase.toClean[i];
+            var instances : Array<ModelItf> = modelToClean.all();
+            for(var j in instances) {
+                var toDelete : ModelItf = instances[j];
+                toDelete.delete();
+            }
+        }
+    }
+
+    /**
+     * Method to fulfill database with some data.
+     *
+     * @method fulfill
+     */
+    fulfill() {
         //TODO
     }
 }
