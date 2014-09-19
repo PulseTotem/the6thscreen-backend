f297a1c5b0f1cf2768dfda01f29f98ee6a105cc0
Simon Urli <simon.urli@gmail.com>
2014-09-19 18:13:58 +0200

    refs #3 implements methods for InfoType

--- app/scripts/model/InfoType.ts
+++ app/scripts/model/InfoType.ts
@@ -92,8 +92,7 @@ class InfoType extends ModelItf {
      */
     sources() {
         if(! this._sources_loaded) {
-            // TODO : Retrieve from database.
-            this._sources_loaded = true;
+            this._sources_loaded = this.getAssociatedObjects(InfoType, Source, this._sources);
         }
         return this._sources;
     }
@@ -103,13 +102,33 @@ class InfoType extends ModelItf {
      */
     renderers() {
         if(! this._renderers_loaded) {
-            // TODO : Retrieve from database.
-            this._renderers_loaded = true;
+            this._renderers_loaded = this.getAssociatedObjects(InfoType, Renderer, this._renderers);
         }
         return this._renderers;
     }
 
     //////////////////// Methods managing model. Connections to database. ///////////////////////////
+
+	loadAssociations() : void {
+		this.renderers();
+		this.sources();
+	}
+
+	toJSONObject() : Object {
+		var data = {
+			"name": this.name()
+		};
+
+		return data;
+	}
+
+	addSource(s : Source) : boolean {
+		return this.associateObject(InfoType, Source, s.getId());
+	}
+
+	addRenderer(r : Renderer) : boolean {
+		return this.associateObject(InfoType, Renderer, r.getId());
+	}
 
     /**
      * Create model in database.
@@ -118,8 +137,7 @@ class InfoType extends ModelItf {
      * @return {boolean} Create status
      */
     create() : boolean {
-        // TODO
-        return false;
+        return this.createObject(InfoType, this.toJSONObject())
     }
 
     /**
@@ -131,8 +149,7 @@ class InfoType extends ModelItf {
      * @return {InfoType} The model instance.
      */
     static read(id : number) : InfoType {
-        // TODO
-        return null;
+        return this.readObject(InfoType, id);
     }
 
     /**
@@ -142,8 +159,7 @@ class InfoType extends ModelItf {
      * @return {boolean} Update status
      */
     update() : boolean {
-        // TODO
-        return false;
+        return this.updateObject(InfoType, this.toJSONObject())
     }
 
     /**
@@ -153,8 +169,7 @@ class InfoType extends ModelItf {
      * @return {boolean} Delete status
      */
     delete() : boolean {
-        // TODO
-        return false;
+        return this.deleteObject(InfoType);
     }
 
     /**
@@ -164,9 +179,36 @@ class InfoType extends ModelItf {
      * @return {Array<InfoType>} The model instances.
      */
     static all() : Array<InfoType> {
-        // TODO
-        return null;
+       return this.allObjects(InfoType);
     }
+
+	/**
+	 * Return an InfoType instance from a JSON string.
+	 *
+	 * @method parseJSON
+	 * @static
+	 * @param {string} json - The JSON string
+	 * @return {Call} The model instance.
+	 */
+	static parseJSON(jsonString : string) : InfoType {
+		return InfoType.fromJSONObject(JSON.parse(jsonString));
+	}
+
+	/**
+	 * Return an InfoType instance from a JSON Object.
+	 *
+	 * @method fromJSONObject
+	 * @static
+	 * @param {JSONObject} json - The JSON Object
+	 * @return {Call} The model instance.
+	 */
+	static fromJSONObject(jsonObject : any) : InfoType {
+		if(typeof(jsonObject.name) == "undefined" || typeof(jsonObject.id) == "undefined") {
+			return null;
+		} else {
+			return new InfoType(jsonObject.name, jsonObject.id);
+		}
+	}
 
     /**
      * Retrieve DataBase Table Name.
@@ -175,7 +217,6 @@ class InfoType extends ModelItf {
      * @return {string} The DataBase Table Name corresponding to Model.
      */
     static getTableName() : string {
-        // TODO
-        return "";
+        return "InfoTypes";
     }
 }
